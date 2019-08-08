0313e828e35f2e28e7f2d550048a12ca8b7da5f9
Sergio Cifuentes <sergiocifuentes1485@gmail.com>
2019-08-08 14:51:26 -0600

    Diagrama De Clases y De Entidad/Relacion

---
+++ src/paqueteria/Paqueteria.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paqueteria;
+
+import paqueteria.ui.IngresoDeUsuario;
+
+/**
+ *
+ * @author sergio
+ */
+public class Paqueteria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        IngresoDeUsuario nuevoUsuario = new IngresoDeUsuario();
+        nuevoUsuario.setVisible(true);
+    }
+    
+}
